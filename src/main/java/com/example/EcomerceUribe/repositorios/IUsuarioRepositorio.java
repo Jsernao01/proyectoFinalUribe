895d31bf8178d5a751e12a8f17206b228363ca50
Juan Jose Gallego <jjosegallegomesa@gmail.com>
2025-11-06 21:59:39 -0500

    ok

--- src/main/java/com/example/EcomerceUribe/repositorios/IUsuarioRepositorio.java
+++ src/main/java/com/example/EcomerceUribe/repositorios/IUsuarioRepositorio.java
@@ -1,6 +1,8 @@
 package com.example.EcomerceUribe.repositorios;
 
 import com.example.EcomerceUribe.modelos.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/example/EcomerceUribe/servicios/UsuarioServicio.java
+++ src/main/java/com/example/EcomerceUribe/servicios/UsuarioServicio.java
@@ -5,9 +5,15 @@ import com.example.EcomerceUribe.modelos.Usuario;
 import com.example.EcomerceUribe.modelos.mapas.IUsuarioMapa;
 import com.example.EcomerceUribe.repositorios.IUsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioServicio {
@@ -57,6 +63,82 @@ public class UsuarioServicio {
         //Retornar el dto al controlador
         return this.mapa.convertir_usuario_a_usuariogenericodto(usuarioQueGuardoElRepo);
     }
+
+    //Buscar todos usuarios (Lista)
+    public List<UsuarioGenericoDTO> buscarTodosLosUsuarios(){
+        List<Usuario> listaDeUsuariosConsultados=this.repositorio.findAll();
+        return this.mapa.convertir_lista_a_listadtogenerico(listaDeUsuariosConsultados);
+    }
+
+    //Buscar Un usuario por ID
+    public UsuarioGenericoDTO buscarUsuarioGenericoPorId(Integer id){
+        Optional<Usuario> usuarioQueEstoyBuscando=this.repositorio.findById(id);
+        if(!usuarioQueEstoyBuscando.isPresent()){
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "No se encontro ningun usuario con el id "+id+" suministrado"
+            );
+        }
+        Usuario usuarioEncontrado = usuarioQueEstoyBuscando.get();
+        return this.mapa.convertir_usuario_a_usuariogenericodto(usuarioEncontrado);
+    }
+
+    //eliminar usuario
+    public void eliminarUsuario(Integer id){
+        Optional<Usuario> usuarioQueEstoyBuscando=this.repositorio.findById(id);
+        if(!usuarioQueEstoyBuscando.isPresent()){
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "No se encontro ningun usuario con el id "+id+" suministrado"
+            );
+        }
+        Usuario usuarioEncontrado = usuarioQueEstoyBuscando.get();
+        try{
+            this.repositorio.delete(usuarioEncontrado);
+        }catch(Exception error){
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    "No se pudo eliminar el usuario, "+error.getMessage()
+            );
+        }
+    }
+
+
+    //modificar algunos datos de un usuario
+    public UsuarioGenericoDTO actualizarUsuario(Integer id, Usuario datosActualizados){
+        Optional<Usuario> usuarioQueEstoyBuscando=this.repositorio.findById(id);
+        if(!usuarioQueEstoyBuscando.isPresent()){
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "No se encontro ningun usuario con el id "+id+" suministrado"
+            );
+        }
+        Usuario usuarioEncontrado = usuarioQueEstoyBuscando.get();
+        //APLIQUE VALIDACIONES SOBRE LOS DATOS QUE MANDARON DESDE EL FRONT
+        //ACTUALIZO LOS CAMPOS QUE SE PERMITIERON MODIFCAR
+        //NOMBRE //CORREO
+
+        usuarioEncontrado.setNombres(datosActualizados.getNombres());
+        usuarioEncontrado.setCorreo(datosActualizados.getCorreo());
+
+        //CONCLUYO LA OPERACION EN LA BASE DE DATOS
+        Usuario usuarioActualizado=this.repositorio.save(usuarioEncontrado);
+
+        if(usuarioActualizado==null){
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    "Error al actualizar el usuario en la base de datos, intenta nuevamente"
+            );
+        }
+
+        return this.mapa.convertir_usuario_a_usuariogenericodto(usuarioActualizado);
+
+
+
+
+
+    }
+
 
 
 
